d2ef4b811edbef635f25532e9ae21b766a63beee
Alex Shaw <alexandergshaw@gmail.com>
2022-07-07 23:28:08 -0500

    wip fix: update url

--- lighthouserc.js
+++ lighthouserc.js
@@ -2,6 +2,9 @@ module.exports = {
   ci: {
     collect: {
       staticDistDir: "./out",
+      url: [
+        "https://alexshaw-static-website.s3.us-east-2.amazonaws.com/index.html",
+      ],
     },
     upload: {
       target: "temporary-public-storage",
@@ -10,8 +13,8 @@ module.exports = {
       assertions: {
         "categories:performance": ["error", { minScore: 0.9 }],
         "categories:accessibility": ["error", { minScore: 0.9 }],
-        "categories:seo": ["error", { minScore: 0.9 }],
         "categories:best-practices": ["error", { minScore: 0.9 }],
+        "categories:seo": ["error", { minScore: 0.9 }],
       },
     },
   },
