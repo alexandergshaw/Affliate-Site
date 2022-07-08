5e52a168441a7198d8af626971cbfd2a80c6b676
Alex Shaw <alexandergshaw@gmail.com>
2022-07-07 23:01:52 -0500

    chore: configure lighthouse ci to run as a Github action

---
+++ lighthouserc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  ci: {
+    collect: {
+      staticDistDir: "./out",
+      url: ['http://localhost:8080']
+    },
+    upload: {
+        target: 'temporary-public-storage',
+    },
+  },
+};
