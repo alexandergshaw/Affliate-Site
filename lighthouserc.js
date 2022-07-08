0f949490f9caf3692c46d8974e17707fd1125548
Alex Shaw <alexandergshaw@gmail.com>
2022-07-07 23:16:48 -0500

    wip fix: remove target server

--- lighthouserc.js
+++ lighthouserc.js
@@ -2,10 +2,17 @@ module.exports = {
   ci: {
     collect: {
       staticDistDir: "./out",
-      url: ['http://localhost:8080']
     },
     upload: {
-        target: 'temporary-public-storage',
+      target: "temporary-public-storage",
+    },
+    assert: {
+      assertions: {
+        "categories:performance": ["error", { minScore: 0.9 }],
+        "categories:accessibility": ["error", { minScore: 0.9 }],
+        "categories:seo": ["error", { minScore: 0.9 }],
+        "categories:best-practices": ["error", { minScore: 0.9 }],
+      },
     },
   },
 };
