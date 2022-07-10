deea6d2cf2b67a721434b9d26e7f830a1706bee3
Alex Shaw <alexandergshaw@gmail.com>
2022-07-09 20:10:39 -0500

    wip fix: updating ready message for lighthouse CI

--- lighthouserc.js
+++ lighthouserc.js
@@ -4,7 +4,7 @@ module.exports = {
       startServerCommand: "npm run start",
       url: ["http://localhost:3000/"],
       startServerReadyPattern:
-        "started server on 0.0.0.0:3000, url: http://localhost:3000",
+        "ready - started server on 0.0.0.0:3000, url: http://localhost:3000",
       startServerReadyTimeout: 20000, // milliseconds
       numberOfRuns: 5,
     },
