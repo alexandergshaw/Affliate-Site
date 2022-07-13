a063c74d0477a1fe8c2bfc5f4782dd1e276789d6
Alex Shaw <alexandergshaw@gmail.com>
2022-07-13 11:26:37 -0500

    wip fix: trying out basic lighthouse config file

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,23 +1,9 @@
 module.exports = {
-  ci: {
-    collect: {
-      startServerCommand: "npm run start",
-      url: ["http://localhost:3000/"],
-      startServerReadyPattern:
-        "ready - started server on 0.0.0.0:3000, url: http://localhost:3000",
-      startServerReadyTimeout: 20000, // milliseconds
-      numberOfRuns: 5,
-    },
-    upload: {
-      target: "temporary-public-storage",
-    },
-    assert: {
-      assertions: {
-        "categories:performance": ["error", { minScore: 0.9 }],
-        "categories:accessibility": ["error", { minScore: 0.9 }],
-        "categories:best-practices": ["error", { minScore: 0.9 }],
-        "categories:seo": ["error", { minScore: 0.9 }],
-      },
-    },
-  },
-};
+  "ci": {
+    "assert": {
+      "assertions": {
+        "first-contentful-paint": ["error", { "minScore": 0.6 }]
+      }
+    }
+  }
+}
